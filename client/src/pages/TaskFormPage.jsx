e7aec7cfa6e7fb3d87e14ae105ad260339bb025f
Leonel <leonelsanz15@gmail.com>
2023-09-22 16:06:46 -0300

    Eliminar tareas

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
-import { createTask } from "../api/tasks.api";
-import { useNavigate } from "react-router-dom";
+import { createTask, deleteTask } from "../api/tasks.api";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function TaskFormPage() {
   const {
@@ -11,9 +11,11 @@ export function TaskFormPage() {
 
   const navigate = useNavigate();
 
+  const params = useParams();
+
   const onSubmit = handleSubmit(async (data) => {
     await createTask(data);
-    navigate('/tasks');
+    navigate("/tasks");
   });
 
   return (
@@ -33,6 +35,20 @@ export function TaskFormPage() {
         {errors.description && <span>Description is required</span>}
         <button>Save</button>
       </form>
+
+      {params.id && (
+        <button
+          onClick={async () => {
+            const accepted = window.confirm("are you sure?");
+            if (accepted) {
+              await deleteTask(params.id);
+              navigate("/tasks");
+            }
+          }}
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 }
